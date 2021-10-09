9453c7e27eef460025861ea90b89b1fca0063e5e
elijah-springer <springer_elijah@yahoo.com>
2021-10-09 17:39:32 -0400

    Initial wav file generation and juce setup.

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,44 @@
+#include <JuceHeader.h>
+#include <iostream>
+#include <stdio.h>
+
+#define JUCE_CHECK_MEMORY_LEAKS 0
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Initializing audio buffer..." << std::endl;
+
+	// Setup Audio Buffer
+	int numChannels = 2;
+	int numSamples = 999999;
+
+	juce::AudioBuffer<float> buffer;
+	buffer.setSize(numChannels, numSamples);
+
+	juce::Random rand;
+
+	for (unsigned int i = 0; i < numSamples; i++) {
+		float chOne = rand.nextFloat() * 0.25f - 0.125f;
+		float chTwo = rand.nextFloat() * 0.25f - 0.125f;
+
+		buffer.addSample(0, i, chOne);
+		buffer.addSample(1, i, chTwo);
+	}
+
+	std::cout << "Starting write..." << std::endl;
+	// Save the wav file
+	juce::File file("E:/Desktop/test.wav");
+	juce::WavAudioFormat format;
+	
+	juce::FileOutputStream outputStream(file);
+
+	juce::ScopedPointer<juce::AudioFormatWriter> writer(format.createWriterFor(&outputStream, 44100, 1, 16, NULL, 0));
 
 
-int main(){
+	writer->writeFromAudioSampleBuffer(buffer, 0, 999999);
 
+	std::cout << "Write completed." << std::endl;
+
+
+    return 0;
 }
